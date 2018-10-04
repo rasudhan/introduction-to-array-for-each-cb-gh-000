54d88ec4355b21f962e7ac58ee382544289125ba
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 23:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ array.forEach(callback);
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
 
-
+ array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 
 }
